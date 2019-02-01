b18592f66a6cc5a07d4665de2657aa30ff95b67a
Andreas Beger <adbeger@gmail.com>
2019-02-01 20:31:07 +0800

    add xgboost results with expanded var set

--- code/xgboost.R
+++ code/xgboost.R
@@ -39,7 +39,10 @@ voi <- c(
 )
 
 num_data <- cy %>%
-  select(gwcode, year, one_of(voi), starts_with("norm_"), starts_with("itt_alleg"), "itt_RstrctAccess") %>%
+  select(gwcode, year, one_of(voi), starts_with("norm_"), starts_with("itt_alleg"), 
+         "itt_RstrctAccess") %>%
+  mutate(gtd_events = scale(log1p(gtd_events))[, 1],
+         gtd_killed = scale(log1p(gtd_killed))[, 1]) %>%
   mutate(gwcode_fct = factor(gwcode))
 # Don't want to center/scale binary/quasi-categorical vars, so ID those
 binary_vars <- num_data %>%
@@ -177,6 +180,8 @@ fit_xgboost <- oos_preds %>%
   select(outcome, model_name, MAE, RMSE, CRPS)
 write_csv(fit_xgboost, "output/xgboost-fit.csv")
 
+
+
 predictor_importance <- mdlX %>%
   map_dfr(., .id = "outcome", function(mm) {
     imp <- varImp(mm)$importance
@@ -245,7 +250,212 @@ pd %>%
   geom_line()
 
 
+
+# xgboost with extended var set -------------------------------------------
+
+
+num_data <- cy %>%
+  select(gwcode, year, one_of(voi), starts_with("norm_"), starts_with("itt_alleg"), 
+         "itt_RstrctAccess", 
+         "gtd_killed", "gtd_events", 
+         starts_with("internal_confl"), starts_with("ext_conf"), starts_with("any_conflict")) %>%
+  mutate(gtd_events = scale(log1p(gtd_events))[, 1],
+         gtd_killed = scale(log1p(gtd_killed))[, 1]) %>%
+  mutate(gwcode_fct = factor(gwcode))
+# Don't want to center/scale binary/quasi-categorical vars, so ID those
+binary_vars <- num_data %>%
+  summarize_all(~ mean(. %in% c(0, 1, 100)) > .9) %>%
+  tidyr::gather(var, quasibinary) %>%
+  filter(quasibinary) %>%
+  pull(var)
+num_data <- recipe(num_data) %>%
+  step_dummy(gwcode_fct) %>%
+  update_role(everything(), new_role = "predictor") %>%
+  update_role(., starts_with("itt_alleg"), new_role = "outcome") %>%
+  update_role(., gwcode, year, new_role = "ID") %>%
+  step_zv(all_predictors()) %>%
+  prep(retain = TRUE)
+train_id_vars <- num_data %>%
+  juice(has_role("ID"))
+train_y <- num_data %>%
+  juice(has_role("outcome"))
+train_x <- num_data %>%
+  juice(has_role("predictor")) %>%
+  as.matrix()
+
+trControl <- caret::trainControl(method = "cv", number = 11, 
+                                 verboseIter = FALSE, savePredictions = TRUE)
+
+# http://xgboost.readthedocs.io/en/latest/how_to/param_tuning.html
+# http://xgboost.readthedocs.io/en/latest/parameter.html
+generate_random_grid <- function(n) {
+  data.frame(nrounds = sample(c(100, 150, 200, 500, 1000), n, replace = TRUE),
+             eta = round(runif(n, 0, .5), 2),
+             # tree complexity
+             max_depth = sample(2:10, n, replace = TRUE),
+             min_child_weight = rpois(n, 1),
+             gamma = round(rexp(n, rate = 5), 1),
+             # randomization
+             colsample_bytree = round(rbeta(n, 3, 1), 2),
+             subsample = round(rbeta(n, 5, 2), 2)
+  )
+}
+fixed_grid <- data.frame(
+  nrounds = 100,
+  eta = .3,
+  max_depth = 5,
+  min_child_weight = 1,
+  gamma = 0,
+  colsample_by_tree = .6,
+  subsample = .75
+)
+hp_grid <- generate_random_grid(200)
+
+mdlX <- list(
+  criminal = caret::train(x = train_x, y = train_y[["itt_alleg_vtcriminal"]],
+                          method = "xgbTree", objective = "count:poisson", 
+                          eval_metric = "poisson-nloglik", trControl = trControl,
+                          tuneGrid = hp_grid, metric = "MAE"),
+  dissident = caret::train(x = train_x, y = train_y[["itt_alleg_vtdissident"]],
+                           method = "xgbTree", objective = "count:poisson", 
+                           eval_metric = "poisson-nloglik", trControl = trControl,
+                           tuneGrid = hp_grid, metric = "MAE"),
+  marginalized = caret::train(x = train_x, y = train_y[["itt_alleg_vtmarginalized"]],
+                              method = "xgbTree", objective = "count:poisson", 
+                              eval_metric = "poisson-nloglik", trControl = trControl,
+                              tuneGrid = hp_grid, metric = "MAE")
+)
+
+write_rds(mdlX, path = "output/models/mdl_xgboost_extvars.rds")
+
+# Hyperparameter tuning
+# Extract all resample predictions
+resample_preds <- mdlX %>% 
+  enframe("outcome") %>%
+  mutate(pred = map(value, "pred")) %>%
+  select(-value) %>%
+  unnest(pred) %>%
+  mutate(pred = as.numeric(pred))
+# ID the hyperparameter group and compute fit metrics
+hp_performance <- resample_preds %>%
+  mutate(hp_group = group_indices(., eta, max_depth, gamma, colsample_bytree, 
+                                  min_child_weight, subsample, nrounds)) %>%
+  group_by(outcome, hp_group) %>%
+  mutate(MAE = mae(obs, pred),
+         RMSE = rmse(obs, pred),
+         CRPS = mean(crps_pois(obs, pred)) ) %>%
+  dplyr::summarize_at(vars(eta:nrounds, MAE:CRPS), unique)
+p <- hp_performance %>%
+  gather(hp, value, eta:nrounds) %>%
+  ggplot(., aes(x = value, y = MAE)) +
+  facet_grid(outcome ~ hp, scales = "free_x") +
+  geom_point() +
+  geom_smooth() +
+  theme_minimal() 
+p
+ggsave(p, file = "output/figures/xgboost-extvars-hp-tuning.png", height = 8, width = 12)
+
+
+
+oos_preds <- mdlX %>%
+  map_dfr(., .id = "yname", function(mm) {
+    out <- mm$pred %>%
+      filter(nrounds   == mm$bestTune$nrounds,
+             max_depth == mm$bestTune$max_depth,
+             eta       == mm$bestTune$eta,
+             gamma     == mm$bestTune$gamma,
+             colsample_bytree == mm$bestTune$colsample_bytree,
+             min_child_weight == mm$bestTune$min_child_weight,
+             subsample == mm$bestTune$subsample)
+    out <- out %>%
+      rename(y = obs, yhat = pred, row_index = rowIndex) %>%
+      select(y, yhat, row_index) %>%
+      mutate(yhat = as.numeric(yhat)) %>%
+      as_tibble()
+    out <- out %>% arrange(row_index) %>% select(-row_index)
+    out
+  })
+
+p <- oos_preds %>%
+  mutate(yname = str_to_title(yname)) %>%
+  ggplot(., aes(x = yhat, y = y)) +
+  facet_wrap(~ yname) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1, linetype = 3) +
+  theme_ipsum() +
+  ggtitle("XGBoost model yhat vs y") 
+p
+ggsave(p, file = "output/figures/xgboost-extvars-y-vs-yhat.png", height = 5, width = 12)
+
+
+fit_xgboost <- oos_preds %>%
+  rename(outcome = yname) %>%
+  group_by(outcome) %>%
+  summarize(MAE = mae(y, yhat),
+            RMSE = rmse(y, yhat),
+            CRPS = mean(crps_pois(y, yhat)) ) %>%
+  mutate(model_name = "XGBoost") %>%
+  select(outcome, model_name, MAE, RMSE, CRPS)
+write_csv(fit_xgboost, "output/xgboost-extvars-fit.csv")
+
+
+
+predictor_importance <- mdlX %>%
+  map_dfr(., .id = "outcome", function(mm) {
+    imp <- varImp(mm)$importance
+    imp <- tibble(predictor = rownames(imp), importance = imp[, 1])
+    imp
+  })
+
+p <- predictor_importance %>%
+  dplyr::filter(!str_detect(predictor, "gwcode_fct")) %>%
+  mutate(predictor = rename_terms(predictor)) %>%
+  group_by(predictor) %>%
+  mutate(avg_imp = mean(importance)) %>%
+  arrange(avg_imp) %>%
+  ungroup() %>%
+  mutate(predictor = factor(predictor) %>% fct_inorder()) %>%
+  ggplot(., aes(x = importance, y = predictor)) +
+  geom_linerangeh(aes(xmin = 0, xmax = importance, y = predictor, group = outcome), 
+                  linetype = 1, color = "gray90", position = position_dodgev(height = .5)) +
+  geom_point(aes(colour = outcome), position = position_dodgev(height = .5)) + 
+  theme_ipsum() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        legend.position = c(.8, .2)) +
+  labs(x = "XGBoost variable importance, 0-100", y = "") +
+  scale_colour_discrete("Outcome")
+p
+ggsave(p, file = "output/figures/xgboost-extvars-variable-importance-v1.png", height = 6, width = 10)
+
+p <- predictor_importance %>%
+  dplyr::filter(!str_detect(predictor, "gwcode_fct")) %>%
+  mutate(predictor = rename_terms(predictor),
+         outcome = str_to_title(outcome)) %>%
+  group_by(predictor) %>%
+  mutate(avg_imp = mean(importance)) %>%
+  arrange(avg_imp) %>%
+  ungroup() %>%
+  mutate(predictor = factor(predictor) %>% fct_inorder()) %>%
+  ggplot(., aes(x = importance, y = predictor)) +
+  geom_linerangeh(aes(xmin = 0, xmax = importance, y = predictor, group = outcome), 
+                  linetype = 1, color = "gray90") +
+  geom_point(aes(colour = outcome)) + 
+  facet_wrap(~ outcome) +
+  theme_ipsum() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        legend.position = "top") +
+  labs(x = "XGBoost variable importance, 0-100", y = "") +
+  scale_colour_discrete("Outcome", guide = FALSE)
+p
+ggsave(p, file = "output/figures/xgboost-extvars-variable-importance-v2.png", height = 6, width = 10)
+
+
+
+
 # Use xgboost on count model residuals ------------------------------------
+
 
 mdl_base1 <- list(
   criminal     = glmer(
