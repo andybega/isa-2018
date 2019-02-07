39b646c2e03de4c7fbdb09bc3b1e80c9144cdece
Andreas Beger <adbeger@gmail.com>
2019-02-07 09:41:23 +0200

    forgot to save file

--- code/3-oos-fit.R
+++ code/3-oos-fit.R
@@ -6,6 +6,8 @@
 
 library("tidyverse")
 library("scoringRules")
+library("ggrepel")
+library("hrbrthemes")
 
 source("code/functions.R")
 
@@ -17,7 +19,9 @@ oos_fit_all <- oos_fit %>%
   mutate(model_name = case_when(
     variable=="none" ~ "Poisson, RE and controls only",
     TRUE ~ "Poisson, RE, controls, and variable")) %>%
-  bind_rows(xgboost_fit)
+  bind_rows(xgboost_fit) %>%
+  mutate(model_name = factor(model_name),
+         model_name = fct_relevel(model_name, "Poisson, RE, controls, and variable"))
 
 # For each fit statistic and outcome, find the best-performing VOI model
 best_voi_model <- oos_fit_all %>%
@@ -25,16 +29,32 @@ best_voi_model <- oos_fit_all %>%
   gather(fitstat, value, MAE:CRPS) %>%
   group_by(fitstat, outcome) %>%
   filter(value==min(value)) %>%
-  mutate(variable = rename_terms(variable))
+  mutate(label = rename_terms(variable)) %>%
+  select(fitstat, outcome, variable, label)
 
-oos_fit_all %>%
-  gather(key, value, MAE:CRPS) %>%
-  ggplot(., aes(x = value, y = outcome, color = model_name)) +
-  facet_wrap(~ key, scales = "free_x") +
-  geom_point(aes(alpha = str_detect(model_name, "(XGBoost)|(controls only)"))) +
-  theme_minimal() +
-  scale_colour_brewer("Model:", type = "qual", palette = 6) +
-  scale_alpha_manual(guide = FALSE, values = c(.2, 1)) +
+df <- oos_fit_all %>%
+  gather(fitstat, value, MAE:CRPS) %>%
+  # join instead of using best_voi_model as separate data because geom_text_repel
+  # only works properly when missing labels are ""
+  left_join(best_voi_model) %>%
+  replace_na(list(label = ""))
+
+df %>%
+  # point drawing is by order in data, so make voi models first so that the 
+  # control models are drawn over them
+  arrange(model_name) %>%
+  ggplot(., aes(x = value, y = outcome)) +
+  facet_wrap(~ fitstat, scales = "free_x") +
+  geom_point(aes(color = model_name,
+                 alpha = str_detect(model_name, "(XGBoost)|(controls only)")),
+             size = 2) +
+  scale_colour_manual("Model:", values = c("#377eb8", "#e41a1c", "#4daf4a")) +
+  scale_alpha_manual(guide = FALSE, values = c(.5, 1)) +
+  # highlight best non control model
+  geom_text_repel(aes(label = label),
+                  nudge_y = .3, size = 3.5, family = "Roboto Condensed",
+                  color = "gray20") +
+  theme_ipsum_rc() +
   labs(x = "Fit statistic value", y = "Outcome variable") +
   theme(legend.position = "top")
-ggsave("output/figures/oos-fit-all.png", height = 3, width = 8)
+ggsave("output/figures/oos-fit-all.png", height = 4.5, width = 10)
